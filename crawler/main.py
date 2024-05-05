b659965ec2b186ff35c4e5d014bba2036402ef66
98yejin <o0o2211915@gmail.com>
2024-05-05 16:36:32 +0900

    refactor : add types and handle error

--- crawler/main.py
+++ crawler/main.py
@@ -1,6 +1,6 @@
 import aiohttp
 import asyncio
-from typing import List
+from typing import List, Optional
 from bs4 import BeautifulSoup
 from dataclasses import dataclass
 
@@ -8,42 +8,33 @@ from dataclasses import dataclass
 @dataclass
 class CrawlResult:
     url: str
-    content: str
+    content: Optional[str] = None
+    error: Optional[str] = None
 
 
-async def fetch_url(session, url) -> CrawlResult:
-    async with session.get(url) as response:
-        response.raise_for_status()
-        html = await response.text()
-        soup = BeautifulSoup(html, "html.parser")
-        text = soup.getText(separator="\n", strip=True)
-        return CrawlResult(url=url, content=text)
-
-
-async def process_url(session, url) -> CrawlResult:
+async def fetch_url(session: aiohttp.ClientSession, url: str) -> CrawlResult:
     try:
-        return await fetch_url(session, url)
-    except aiohttp.ClientResponseError as e:
-        print(f"[ERROR] ClientResponseError: [{e}]")
-    except aiohttp.ClientError as e:
-        print(f"[ERROR] ClientError: [{e}]")
-    except aiohttp.ClientTimeout as e:
-        print(f"[ERROR] ClientTimeout: [{e}]")
+        async with session.get(url) as response:
+            response.raise_for_status()
+            html = await response.text()
+            soup = BeautifulSoup(html, "html.parser")
+            text = soup.getText(separator="\n", strip=True)
+            return CrawlResult(url=url, content=text)
     except Exception as e:
-        print(f"[ERROR] Exception: [{e}]")
-    return CrawlResult(url=url, content="")
+        print(f"[ERROR] {type(e).__name__}: {e}")
+        return CrawlResult(url=url, error=str(e))
 
 
 async def process_batch(urls: List[str], timeout_seconds=3) -> List[CrawlResult]:
     timeout = aiohttp.ClientTimeout(total=timeout_seconds)
     async with aiohttp.ClientSession(timeout=timeout) as session:
         results = await asyncio.gather(
-            *[process_url(session=session, url=url) for url in urls]
+            *[fetch_url(session=session, url=url) for url in urls]
         )
         return results
 
 
-async def main(urls, batch_size=10):
+async def main(urls: List[str], batch_size=10):
     batches = [urls[i : i + batch_size] for i in range(0, len(urls), batch_size)]
     return await asyncio.gather(*[process_batch(batch) for batch in batches])
 
@@ -53,7 +44,7 @@ if __name__ == "__main__":
         "http://example.com/page1",
         "http://example.com/page2",
         "http://example.com/page3",
-        "https://98yejin.github.io",
+        # "https://98yejin.github.io",
     ]
     results = asyncio.run(main(urls))
     print(results)
